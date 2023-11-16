e34beb4a905d8d21bfc8cee06903147e71e71a51
benjamom <ben.duranr@duocuc.cl>
2023-11-16 18:22:20 -0300

    first commit

---
+++ Ecoubica/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class location(models.Model):
+    name = models.CharField(max_length=250, verbose_name = 'Punto Verde')
+    adress = models.CharField(max_length=250, verbose_name='Direccion')
+    state = models.CharField(max_length=250, verbose_name='Estado')
+    lat = models.FloatField(verbose_name='Latitud')
+    lng = models.FloatField(verbose_name='Longitud')
+
+    class Meta:
+        verbose_name = 'Punto Verde'
+        verbose_name_plural='Puntos Verdes'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name    
---
+++ Ecoubica/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import folium
+from .models import location as LocationModel
+
+def home(request):
+
+    locations = LocationModel.objects.all()
+
+    initialMap = folium.Map(location=[-33.5000852,-70.6162928], zoom_start=14)
+
+    for loc in locations:
+        coordinates =(loc.lat, loc.lng)
+        folium.Marker(coordinates, popup = 'Sucursal: '+ loc.name).add_to(initialMap)
+
+    context ={'map': initialMap._repr_html_(), 'locations':locations}
+    return render(request, 'map/home.html', context)
